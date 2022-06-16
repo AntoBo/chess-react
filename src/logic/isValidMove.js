540b1051727f5c708885a3cc6022701dd7d5f850
AntoBo <anton.bondarenkooooo@gmail.com>
2022-06-16 23:11:08 +0300

    pawns range done

--- src/logic/isValidMove.js
+++ src/logic/isValidMove.js
@@ -1,5 +1,5 @@
 const letters = ["a", "b", "c", "d", "e"];
-let moveTo, moveFrom, board, turn;
+let moveTo, moveFrom, fromLet, fromNum, toLet, toNum, board, turn;
 
 const isValidMove = ({ move, incomingBoard, incomingTurns }) => {
   if (move.length < 4) {
@@ -8,6 +8,10 @@ const isValidMove = ({ move, incomingBoard, incomingTurns }) => {
 
   moveFrom = move.slice(0, 2);
   moveTo = move.slice(2);
+  fromLet = letters.indexOf(moveFrom[0]) + 1;
+  fromNum = Number(moveFrom[1]);
+  toLet = letters.indexOf(moveTo[0]) + 1;
+  toNum = Number(moveTo[1]);
   board = incomingBoard;
   turn = incomingTurns.length % 2 === 0 ? "white" : "black";
 
@@ -56,15 +60,26 @@ const isRangeOk = () => {
   }
   return isRangeOk;
 };
-const isKingsRangeOk = () => {
-  const fromLet = letters.indexOf(moveFrom[0]) + 1;
-  const fromNum = Number(moveFrom[1]);
+const isKingsRangeOk = () =>
+  Math.abs(fromNum - toNum) <= 1 && Math.abs(fromLet - toLet) <= 1;
 
-  const toLet = letters.indexOf(moveTo[0]) + 1;
-  const toNum = Number(moveTo[1]);
-  return Math.abs(fromNum - toNum) <= 1 && Math.abs(fromLet - toLet) <= 1;
+const isPawnsRangeOk = () => {
+  const dir = turn === "white" ? 1 : -1;
+  //only white pawn!
+  const canMove = () =>
+    fromNum === toNum &&
+    toLet - fromLet === 1 * dir &&
+    Boolean(!board[moveTo].rank);
+  const canHit = () => {
+    return (
+      Boolean(board[moveTo].armyColor) &&
+      Boolean(board[moveTo].armyColor !== turn) &&
+      Math.abs(fromLet - toLet) <= 1 &&
+      Math.abs(fromNum - toNum) === 1
+    );
+  };
+  return canMove() || canHit();
 };
-const isPawnsRangeOk = () => {};
 const isRooksRangeOk = () => {};
 
 export default isValidMove;
